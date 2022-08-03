827b51655e6dafffae82de04d0e847e45e6c26bb
Adrian <adrianvalente1@gmail.com>
2022-08-03 11:14:01 -0300

    bom samaritanos

--- bomSamaritanos/assets/js/main.js
+++ bomSamaritanos/assets/js/main.js
@@ -16,3 +16,30 @@ var carousel = new bootstrap.Carousel(myCarousel, {
   interval: 2000,
   wrap: false
 })
+
+
+
+google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Anos', 'Volntários', 'Projetos' , 'Doações'],
+          ['2018-2',  1        ,  1         ,  0        ],
+          ['2019-1',  5        ,  1         ,  0        ],
+          ['2019-2',  47       ,  2         ,  5        ],
+          ['2020-1',  60       ,  2         ,  20       ]
+        ]);
+
+        var options = {
+          vAxis: {minValue: 0},
+          lineWidth: 5,
+          series: {
+            0: { color: '#0ab4af' },
+            1: { color: '#db9902' },
+            2: { color: '#1464e9' },
+          }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
